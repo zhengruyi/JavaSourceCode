8316f7b70b8a02ac4a87f38b4fc6d1a0adcb0499
Ruyi ZHENG <standonthpeak@gmail.com>
2021-02-08 14:51:55 +0100

    push jdk1.8 source code

---
+++ src/test/ObjectTest.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author Ruyi ZHENG
+ * @version 1.00
+ * @time 08/02/2021 14:04
+ **/
+
+public class ObjectTest {
+    public static void main(String[] args) {
+        Object o = new Object();
+        System.out.println(o.toString());
+    }
+}
